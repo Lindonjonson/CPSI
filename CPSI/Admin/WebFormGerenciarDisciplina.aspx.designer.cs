fc061a3223ef6da7886cd3a26956dbfc9c6f9033
Weslley Alves Vieira <weslleyalvesv13@gmail.com>
2019-03-26 14:17:08 -0300

    Tratamento de entrada do form documento e disciplina

--- CPSI/Admin/WebFormGerenciarDisciplina.aspx.designer.cs
+++ CPSI/Admin/WebFormGerenciarDisciplina.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace CPSI.Admin.Disciplina {
         protected global::System.Web.UI.WebControls.GridView GridViewDisciplina;
         
         /// <summary>
+        /// Controle RequiredFieldValidator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
         /// Controle TxtNomeDisciplina.
         /// </summary>
         /// <remarks>
